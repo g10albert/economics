0354345caa1ef46907edef2dc2891bf347385207
g10albert <albertmiguelg10@gmail.com>
2022-11-03 18:16:10 -0400

    I created the home page for mobile and started the styles for the header

---
+++ js/index.js
@@ -0,0 +1,52 @@
+const body = document.querySelector('body');
+const initialTheme = 'Light';
+const toggleTheme = document.querySelector('#toggle-theme');
+const menu = document.querySelector('.nav')
+const openMenu = document.querySelector('.nav__btn-open')
+const closeMenu = document.querySelector('.nav__btn-close')
+
+// Making menu responsive
+
+openMenu.addEventListener('click', () => {
+    menu.classList.remove('hidden')
+    openMenu.classList.toggle('hidden')
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.add('hidden')
+    openMenu.classList.toggle('hidden')
+})
+
+
+// Creating the dark/light theme
+
+const setTheme = (theme) => {
+    localStorage.setItem('theme', theme);
+    body.setAttribute('data-theme', theme);
+}
+
+toggleTheme.addEventListener('click', () => {
+
+    activeTheme = localStorage.getItem('theme');
+
+    if (activeTheme === 'light') {
+        setTheme('dark');
+    } else {
+        setTheme('light');
+    }
+})
+
+const setThemeOnInit = () => {
+    const savedTheme = localStorage.getItem('theme');
+
+    activeTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+        body.setAttribute('data-theme', savedTheme);
+
+    } else {
+        setTheme(initialTheme);
+    }
+}
+
+setThemeOnInit();
